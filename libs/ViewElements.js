bbc4cf839a7961cd5cb96ff719d0aec8add0e295
MiaoShi09 <miao@aion.network>
2019-08-05 13:20:40 +0800

    add comments to ViewElement; update namemapping files

--- libs/ViewElements.js
+++ libs/ViewElements.js
@@ -1,4 +1,4 @@
-const validationElem = require("../validationElemNameMapping.json");
+const validationElem = require("../mappedViews/validationElemNameMapping.json");
 const LOADING_TIMEOUT= 8000;
 
 
@@ -26,14 +26,18 @@ class ViewElements{
     this.os = os.toLowerCase() || "android";
 
 
-    this.mapped_views = ["logInPage","registerPage"];
+    this.mapped_views = ["logInPage","registerPage","mnemonicInitPage","mainMenu","settingsPage"];
     this.views={};
     this.app = browser;
   }
-
+  /**
+ * a function to map the essential objects on the current page; the properties of each object must be stored in a json file in "mappedViews" folder
+ * @param {string} pageName the name of a current page which should be lined up with the json file in "mappedViews" folder.
+ * @return {object} contains all the mapped objects on the given page
+ */
   async loadPage(pageName){
     if(this.mapped_views.includes(pageName)){
-      let pageElemData = require("../"+pageName+"NameMapping.json");
+      let pageElemData = require("../mappedViews/"+pageName+"NameMapping.json");
       var self = this;
       //console.log(self);
       //console.log(self.views);
@@ -50,22 +54,43 @@ class ViewElements{
     }
   }
 
+  /**
+ * a function to wait and validate if the app is loading on the expected section
+ * @param {string} mapped_elem_name the name of the object that stored on "mappedViews/validationElemNameMapping.json"
+ * @return {Promise} element.waitForExist()
+ */
+
   async isLoaded(mapped_elem_name){
     var self = this;
 
     return (await checkElement(self.lang,self.os, validationElem[mapped_elem_name], self.app)).waitForExist(LOADING_TIMEOUT);
   }
 
+  /**
+  * a function to get a given element on the view
+  * @param {string} mapped_elem_name the name of the object that stored on "mappedViews/validationElemNameMapping.json"
+  * @return {Promise} element
+  */
   async getOrphanElem(mapped_elem_name){
     var self  = this;
     return checkElement(self.lang,self.os, validationElem[mapped_elem_name], self.app);
   }
+
 
   async validateNameMapping(pageName){
 
   }
 }
 
+/**
+* a private recursive function to go over all the objects in a name mapping file
+* @param {object} map where to store all the elements
+* @param {string} lang the language of the application
+* @param {os} os the operating system the current application is running on
+* @param {Array<object>} data a list of NameMapping properites 
+* @param {webdriver.browser} app that connected with the target application
+* @param {number} i index of the current data position
+*/
 
 async function iteratorPageElement(map,lang,os,data,app,i){
   if(i == data.length){
